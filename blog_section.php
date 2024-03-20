2c1e46615a7c7c84295d7137d50632363d2887f1
Adarshjaiswal <krishj067@gmail.com>
2024-03-20 19:23:09 +0530

    blog new

--- blog_section.php
+++ blog_section.php
@@ -12,27 +12,27 @@
 
     <div class="row">
       <div class="col-md-4">
-        <a href="https://legaltax.in/blog/how-to-file-gst-in-india-in-detail/">
+        <a href="https://legaltax.in/blog/trademark-class-44/">
           <div class="b-img-n">
-            <img src="img/gst-flie-india.jpeg" alt="gst-flie-india">
+            <img src="img/Health-and-Beauty-Services.jpg" alt="gst-flie-india">
           </div>
         </a>
       </div>
 
 
       <div class="col-md-4">
-        <a href="https://legaltax.in/blog/trademark-class-1-chemical-products/">
+        <a href="https://legaltax.in/blog/all-you-need-to-know-about-isi-mark-and-agmark-certification/">
           <div class="b-img-n">
-            <img src="img/Trademark-chemical-products.jpeg" alt="Trademark-chemical-products">
+            <img src="img/ISI-Mark-and-AGMARK-certification.jpg" alt="Trademark-chemical-products">
           </div>
         </a>
       </div>
 
 
       <div class="col-md-4">
-        <a href="https://legaltax.in/blog/how-to-register-one-person-company/">
+        <a href="https://legaltax.in/blog/e-way-bill-limit-and-invoice-system-2023/">
           <div class="b-img-n">
-            <img src="img/how to register-opc.jpeg" alt="how to register-opc">
+            <img src="img/E-Way-Bill-Invoice-System-2023.jpg" alt="how to register-opc">
           </div>
         </a>
       </div>
